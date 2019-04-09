6174c5dec540cd6c60a873917fee0d292f820f04
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2019-04-09 15:24:55 +0200

    Add bootstrap

--- core/settings.py
+++ core/settings.py
@@ -135,7 +135,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+# STATIC_ROOT = 'static'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
 
 ########################################################################################################################
 #                                         LDAP Config                                                                  #
