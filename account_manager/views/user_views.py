635933e32ce78016b6d9a84a2a0e75db7484aedf
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2019-04-11 14:34:01 +0200

    Fix error on user add without default group

--- account_manager/views/user_views.py
+++ account_manager/views/user_views.py
@@ -61,7 +61,8 @@ def user_add(request, realm_id):
             user = LdapUser.objects.get(username=username)
             LdapGroup.base_dn = f'ou=groups,{realm.ldap_base_dn}'
             default_ldap_group = LdapGroup.objects.get(name=realm.default_group.name)
-            ldap_add_user_to_groups(ldap_user=user.dn, user_groups=[default_ldap_group, ])
+            if default_ldap_group:
+                ldap_add_user_to_groups(ldap_user=user.dn, user_groups=[default_ldap_group, ])
             return redirect('realm-user-list', realm_id)
 
     # if a GET (or any other method) we'll create a blank form
