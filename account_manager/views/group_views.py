b8b7fea7adf83079df9cee8c2ec4b3f9d0f03b11
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2019-04-11 14:35:41 +0200

    Add info if group could not be created, because no member (ldap requirement)

--- account_manager/views/group_views.py
+++ account_manager/views/group_views.py
@@ -44,7 +44,7 @@ def group_detail(request, realm_id, group_dn):
 @login_required
 @is_realm_admin
 def group_add(request, realm_id):
-    realm_obj = Realm.objects.get(id=realm_id)
+    realm = Realm.objects.get(id=realm_id)
     LdapUser.base_dn = LdapUser.ROOT_DN
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
@@ -52,18 +52,19 @@ def group_add(request, realm_id):
         form = AddLDAPGroupForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            name = form.cleaned_data['name']
+            name = form.cleaned_data["name"]
             members = form.cleaned_data['members']
             members = [member.dn for member in members]
-            LdapGroup.base_dn = f'ou=groups,{realm_obj.ldap_base_dn}'
+            LdapGroup.base_dn = f'ou=groups,{realm.ldap_base_dn}'
             LdapGroup.objects.create(name=name, members=members)
             return redirect('realm-group-list', realm_id)
-
+        elif 'members' not in form.cleaned_data:
+            return render(request, 'group/group_add.jinja2', {'form': form, 'realm': realm,
+                                                              'extra_error': 'Bitte wähle mindestens ein Mitglied aus. Falls kein Mitglied angezeigt wird, erstelle zuerst einen Nutzer'})
     # if a GET (or any other method) we'll create a blank form
     else:
         form = AddLDAPGroupForm()
-
-    return render(request, 'group/group_add.jinja2', {'form': form, 'realm': realm_obj})
+    return render(request, 'group/group_add.jinja2', {'form': form, 'realm': realm})
 
 
 @login_required
