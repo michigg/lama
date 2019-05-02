1a14db16b43301ab239d2fe678db6a4c2a13c388
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2019-05-02 13:53:34 +0200

    Init translation

--- src/core/settings.py
+++ src/core/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,7 +122,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de_fr'
+LANGUAGES = (
+    ('en', _('English')),
+    ('de', _('German')),
+    ('de-fr', _('German (Frankish')),
+)
 
 TIME_ZONE = 'UTC'
 
@@ -130,6 +136,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale')
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
