7cd3d1ed7e2edbac3dc41d312c3a30d21dd1e0ee
michigg <michael-guenther.goetz@stud.uni-bamberg.de>
2019-11-14 19:23:19 +0100

    Fix wrong selected settings

--- src/account_manager/utils/mail_utils.py
+++ src/account_manager/utils/mail_utils.py
@@ -10,18 +10,16 @@ from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.conf import settings
 
-from core.settings import EMAIL_HOST, EMAIL_PORT, EMAIL_USE_SSL, EMAIL_USE_TLS
-
 logger = logging.getLogger(__name__)
 
 
 def realm_send_mail(realm, to, subject, message):
     logger.info('send mail')
-    connection = get_connection(host=EMAIL_HOST,
-                                port=EMAIL_PORT,
+    connection = get_connection(host=settings.EMAIL_HOST,
+                                port=settings.EMAIL_PORT,
                                 username=realm.email,
-                                use_ssl=EMAIL_USE_SSL,
-                                use_tls=EMAIL_USE_TLS)
+                                use_ssl=settings.EMAIL_USE_SSL,
+                                use_tls=settings.EMAIL_USE_TLS)
     send_mail(subject=subject,
               message=strip_tags(message),
               html_message=message,
