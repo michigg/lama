4d53666189f0cb395a382d4c2b8e866b690b8e5d
Michael Götz <michael.goetz@lion5.io>
2020-04-10 03:26:18 +0200

    chore: update docker setup. Improve for future vue implementation

--- backend/docker/lama/entrypoint.test.sh
+++ backend/docker/lama/entrypoint.test.sh
@@ -3,7 +3,5 @@ echo "-------- make migrations --------"
 python3 /lama/manage.py makemigrations account_helper
 echo "-------- migrate --------"
 python3 /lama/manage.py migrate
-echo "-------- generate test data --------"
-python3 /lama/manage.py generate_test_data
-echo "-------- start server --------"
-python3 /lama/manage.py runserver 0.0.0.0:80
+echo "-------- start test --------"
+python3 /lama/manage.py test
